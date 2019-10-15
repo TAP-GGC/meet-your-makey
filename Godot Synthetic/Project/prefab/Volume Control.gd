6930f9dbb0e25e04ed25350a6240c678cca009ad
Valter-Wulfric-Sherbune-Jr <hiiamaalien@gmail.com>
2019-10-15 13:13:34 -0400

    backup

--- Godot Synthetic/Project/prefab/Volume Control.gd
+++ Godot Synthetic/Project/prefab/Volume Control.gd
@@ -5,22 +5,34 @@ export(NodePath) onready var texture_ref = get_node(texture_ref)
 export(NodePath) onready var label_ref = get_node(label_ref)
 export(NodePath) onready var slider_ref = get_node(slider_ref)
 
-export(Dictionary) onready var texture_mode = { "volume0" : Texture, "volume1" : Texture, 
-"volume2" : Texture, 
-"volume3" : Texture, }
+export(Texture) onready var volume_0
+export(Texture) onready var volume_1
+export(Texture) onready var volume_2
+export(Texture) onready var volume_3
 
-export(Texture) onready var test
+var volume
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	#Set Default Value
 	label_ref.text = str("Volume: ", slider_ref.value)
-	var test2 = (texture_mode.get("volume0"))
-	texture_ref.texture = test2
-	#texture_ref.texture = test
-	pass # Replace with function body.
+	volume = slider_ref.value
+	texture_ref.texture = volume_1
+	pass
 
 func _on_HSlider_value_changed(value):
-	if(value == 0):
-		texture_ref.texture = texture_mode["volume0"]
+	#Update with new value if value change
+	_set_volume_icon(value)
 	label_ref.text = str("Volume: ", value)
-	pass # Replace with function body.
+	volume = value
+	pass
+	
+func _set_volume_icon(value):
+	if(value == 0):
+		texture_ref.texture = volume_0
+	elif(value <= 33):
+		texture_ref.texture = volume_1
+	elif(value <= 66):
+		texture_ref.texture = volume_2
+	elif(value <= 100):
+		texture_ref.texture = volume_3
